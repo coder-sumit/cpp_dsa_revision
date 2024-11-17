24ed078167c81472995e8c6bc8999b284f83bb10
sumit <studyzone.sumit@gmail.com>
2024-11-17 18:58:49 +0530

    Add Eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+    eslint.configs.recommended, // Base ESLint config
+    {
+        files: ['**/*.ts', '**/*.tsx'], // Apply to TypeScript files
+        languageOptions: {
+            parser: tsParser, // Use TypeScript parser
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                project: './tsconfig.json', // Ensure this points to your tsconfig.json
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            // Override and ensure the rule is correctly set
+            '@typescript-eslint/no-unused-expressions': [
+                'error',
+                {
+                    allowShortCircuit: true,
+                    allowTernary: true,
+                    allowTaggedTemplates: true,
+                },
+            ],
+            'no-undef': 'off', // To fix 'console' is not defined issue
+            'no-console': 'off',
+            'dot-notation': 'error',
+        },
+    },
+    {
+        ignores: ['dist', 'node_modules', 'eslint.config.mjs'], // Ignore dist and node_modules
+    },
+];
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-function welcome(name: String) {
+function welcome(name: string) {
     console.log('Welcome to Pizza App!' + name);
 }
+
+const user = {
+    name: 'Sumit',
+};
+
+const u1 = user.name;
+
+welcome(u1);
 
 welcome('Sumit');
